ee98447ccb3cc012962fa0726db04282360202da
matheus <cardosomcds1@gmail.com>
2023-12-11 08:17:51 -0300

    teste de datatypes.

--- table_inserts.py
+++ table_inserts.py
@@ -10,21 +10,25 @@ list_tuple = [tuple(row[1]) for row in data.iterrows()]
 split_data = [list_tuple[i: i+ 100] for i in range(0, len(list_tuple),100)]
 
    
-#connection = start_conection()
+connection = start_conection()
 #print(split_data[0])
 
-#data.info()
-
-#dtype_phone = {}
 
 
-#with connection:
-#    for data_100 in split_data:
-#        insert_phone = """INSERT INTO TB_TB_TELEFONE (TIPO, TELEFONE, DDD) VALUES (:1, :2 :3)"""
-#        
-#        cursor = connection.cursor()        
-#        cursor.executemany(insert_phone, data_100)
-#        connection.commit()
+
+data['tipo']     = data['tipo'].astype('string')
+data['telefone'] = data['telefone'].astype('string')
+data['ddd']      = data['ddd'].astype('string') 
+
+data.info()
+
+with connection:
+    for data_100 in split_data:
+        insert_phone = """INSERT INTO TB_TELEFONE (TIPO, TELEFONE, DDD) VALUES (:1, :2 :3)"""
+        
+        cursor = connection.cursor()        
+        cursor.executemany(insert_phone, data_100)
+        connection.commit()
         
         
         
