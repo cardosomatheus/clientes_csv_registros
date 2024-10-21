6c4c5e85f4ccb52e8f657dce1665cc72fe827c73
matheus <cardosomcds1@gmail.com>
2024-10-21 16:19:04 -0300

    Refatorando o processo de cria tabelas.

---
+++ create_database/cliente.py
@@ -0,0 +1,7 @@
+﻿import os
+from dotenv import load_dotenv
+
+load_dotenv()
+var_user = os.getenv('USER')
+
+print(var_user)
---
+++ create_database/connection_oracle.py
@@ -0,0 +1,12 @@
+﻿import oracledb 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+var_user     = os.getenv('USER')
+var_password = os.getenv('PASSWORD')
+var_dsn      = os.getenv('DSN')
+
+
+def connection ():
+  return oracledb.connect(user=var_user, password=var_password, dsn=var_dsn)
